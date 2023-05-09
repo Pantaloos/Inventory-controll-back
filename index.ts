863271e1732030bb3c6aa36631a73384ea3f0b33
Pantaloos <irakli.kubaneishvili00@gmail.com>
2023-05-09 15:43:17 +0400

    add paginations, error handling and sorting

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import db from './models';
+import express from "express";
+import db from "./models";
 import cors from "cors";
 
 const port = process.env.PORT || 5000;
@@ -8,28 +8,44 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/items', async (req, res) =>{
-    const items = await db.Item.findAll({ limit: 1000 });
-    res.json(items);
-    console.log(items)
-})
+const PAGE_SIZE = 20;
 
-app.post('/items', async (req, res) =>{
-    console.log(req)
-    const {location, name, price} = req.body;
-    await db.Item.create({location, name, price });
+app.get("/inventory/:page", async (req, res) => {
+  const { page } = req.params;
+  const { locationName, columnName, order } = req.query;
+  const itemsInfo = await db.Item.findAndCountAll({
+    where: {
+      ...(locationName ? { location: locationName } : {}),
+    },
+    ...(columnName && order ? { order: [[columnName, order]] } : {}),
+    limit: PAGE_SIZE,
+    offset: PAGE_SIZE * (parseInt(page) - 1),
+  });
+  res.json(itemsInfo);
 });
 
-app.delete('/items/:id',async (req, res) => {
-    const {id} = req.params;
-    await db.Item.destroy({where:{id}});
-    res.send();
-})
+app.post("/inventory", async (req, res) => {
+  console.log(req);
+  const { location, name, price } = req.body;
 
-db.sequelize.sync().then(()=>{
-    app.listen(port, ()=>{
-        console.log(`listening to port ${port}`)
-    })
-})
+  try {
+    const result = await db.Item.create({ location, name, price });
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.delete("/inventory/:id", async (req, res) => {
+  const { id } = req.params;
+  await db.Item.destroy({ where: { id } });
+  res.send();
+});
+
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+  });
+});
 
 module.exports = app;
